264926cfd4e439ebc282726e47cd8bcd024ba528
Siti Norain Ishak <siti.norain.ishak@gmail.com>
2021-06-19 19:10:20 +0800

    Create contact model class

---
+++ contact/contact/Classes/Model/Contact.swift
@@ -0,0 +1,16 @@
+//
+//  Contact.swift
+//  contact
+//
+//  Created by Siti Norain Ishak on 19/06/2021.
+//
+
+import Foundation
+
+struct Contact: Codable {
+    let id: String
+    let firstName: String
+    let lastName: String
+    let email: String?
+    let phone: String?
+}
